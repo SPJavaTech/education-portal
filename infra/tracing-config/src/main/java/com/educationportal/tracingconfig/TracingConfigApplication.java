9a57d915f08c5ae013dcde4c37659caa2abfa527
snpanchal <spjavainfo@gmail.com>
2025-08-12 18:19:58 +0530

    Phase-0  infra/resilience4j-config and infra/trace-config skeleton

---
+++ infra/tracing-config/src/main/java/com/educationportal/tracingconfig/TracingConfigApplication.java
@@ -0,0 +1,13 @@
+package com.educationportal.tracingconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingConfigApplication.class, args);
+	}
+
+}
---
+++ infra/tracing-config/src/test/java/com/educationportal/tracingconfig/TracingConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationportal.tracingconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
