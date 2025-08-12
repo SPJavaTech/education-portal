723b3003162ab0c6f3bc07c307c058347b56c0eb
snpanchal <spjavainfo@gmail.com>
2025-08-12 18:08:49 +0530

    Phase-0  common/common-logging skeleton

---
+++ common/common-logging/src/test/java/com/educationportal/commonlogging/CommonLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationportal.commonlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
